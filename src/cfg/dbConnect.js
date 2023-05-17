b48b77818b62c59db3dae389afe7d9a60ea83f15
cvtrsy <vieira.n55@gmail.com>
2023-05-17 17:58:17 -0300

    conexao com o banco alterada

--- src/cfg/dbConnect.js
+++ src/cfg/dbConnect.js
@@ -5,10 +5,10 @@ import mongoose from "mongoose";
 
 //conexão com o banco local
 //alterar mongod.conf acrescentar bindIp: 10.1.48.196,127.0.0.1 e tambem liberar a porta 27017
-mongoose.connect("mongodb://10.1.48.196:27017/alura-node");
+//mongoose.connect("mongodb://10.1.48.196:27017/alura-node");
 
 //conexão com o banco no Atlas
-//mongoose.connect("mongodb+srv://alura:15911@cluster0.bbho8n.mongodb.net/alura-node?retryWrites=true&w=majority");
+mongoose.connect("mongodb+srv://alura:15911@cluster0.bbho8n.mongodb.net/alura-node?retryWrites=true&w=majority");
 
 // vamos encapsular essa conexão em uma variavel
 let db = mongoose.connection;
